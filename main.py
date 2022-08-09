df24f1e46e791e29f37b7a684204e807f166817f
ChuvashevRoman <82436431+ChuvashevRoman@users.noreply.github.com>
2022-08-09 22:35:41 +0300

    with NN

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ x_steps_1 = []
 y_steps_1 = []
 
 for item in steps_1[x_coll].to_numpy():
-    x_steps_1.append(item.reshape(20,20))
+    x_steps_1.append(item.reshape(20, 20))
 for item in steps_1[y_coll].to_numpy():
-    y_steps_1.append(item.reshape(20,20))
+    y_steps_1.append(item.reshape(20, 20))
+
+# Матрица входных данных
+X_matrix = x_steps_1[0]
+
+# Матрица выходных данных
+Y_matrix = y_steps_1[0]
+
